d10290a564cdae1ef6669efb7ebb0398c86b7fd9
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-11-04 03:38:01 -0400

    HW3 : Full duplex encrypted communication verified with third echo server
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- ASSIGNMENT3/src/main.c
+++ ASSIGNMENT3/src/main.c
@@ -6,6 +6,9 @@
 #include <pb_aes.h>
 #include <pb_sc.h>
 
+#define TRANSP_SERVER	1
+#define KEY_ENABLE	1
+
 #define BUF_SIZE	1024
 
 in_args_t in_args;
@@ -18,44 +21,72 @@ int main(int argc, char *argv[])
 		printf(" Invalid Arguments\n");
 		return 0;
 	}
-	print_args();
+	//print_args();
 
 	/* Server */
 	if (get_flag(ARG_FLAG_LPORT)) {
-		uint8_t server_in_buf[1024] = {0};
-		uint8_t server_out_buf[1024] = {0};
+		uint8_t server_in_buf[BUF_SIZE + 16] = {0};
+		uint8_t server_out_buf[BUF_SIZE + 16] = {0};
 		int size;
 		uint8_t *iv = get_iv();
 		int server_sock = create_serv_sock(in_args.local_port);
-		fcntl(STDIN_FILENO,F_SETFL,O_NONBLOCK);
-		fcntl(server_sock,F_SETFL,O_NONBLOCK);
+#if !TRANSP_SERVER
+		int client_sock = create_client_sock(in_args.dest_port, in_args.dest_name);
+		fcntl(client_sock, F_SETFL, O_NONBLOCK);
+#endif
+		fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+		fcntl(server_sock, F_SETFL, O_NONBLOCK);
 		while (1) {
+#if TRANSP_SERVER
 			size = read(STDIN_FILENO, server_in_buf, BUF_SIZE);
 			if (size > 0) {
-				memcpy(iv, server_in_buf, 8);
 				iv = get_iv();
 				memcpy(server_out_buf, iv, 8);
 				aes_ctr_encrypt(server_in_buf, server_out_buf + 8, iv, size);
 				write(server_sock, server_out_buf, size + 8);
 			}
-
 			size = read(server_sock, server_in_buf, BUF_SIZE);
 			if (size > 8) {
 				memcpy(iv, server_in_buf, 8);
 				aes_ctr_encrypt(server_in_buf + 8, server_out_buf, iv, size - 8);
 				write(STDOUT_FILENO, server_out_buf, size - 8);
 			}
+#elif KEY_ENABLE
+			size = read(server_sock, server_in_buf, BUF_SIZE);
+			if (size > 0) {
+				memcpy(iv, server_in_buf, 8);
+				aes_ctr_encrypt(server_in_buf + 8, server_out_buf, iv, size - 8);
+				write(client_sock, server_out_buf, size - 8);
+				size = read(client_sock, server_in_buf, BUF_SIZE);
+				if (size > 0) {
+					iv = get_iv();
+					memcpy(server_out_buf, iv, 8);
+					aes_ctr_encrypt(server_in_buf, server_out_buf + 8, iv, size);
+					write(server_sock, server_out_buf, size + 8);
+				}
+			}
+#else
+			size = read(server_sock, server_in_buf, BUF_SIZE);
+			if (size > 0) {
+				write(client_sock, server_in_buf, size);
+				size = read(client_sock, server_in_buf, BUF_SIZE);
+				if (size > 0) {
+					write(server_sock, server_in_buf, size);
+				}
+			}
+#endif
 		}
 	/* Client */
 	} else {
-		char client_in_buf[1024] = {0};
-		char client_out_buf[1024] = {0};
+		char client_in_buf[BUF_SIZE + 16];
+		char client_out_buf[BUF_SIZE + 16];
 		int size;
 		uint8_t *iv = get_iv();
-		int client_fd = create_client_sock(8080, NULL);
-		fcntl(STDIN_FILENO,F_SETFL,O_NONBLOCK);
-		fcntl(client_fd,F_SETFL,O_NONBLOCK);
+		int client_fd = create_client_sock(in_args.dest_port, in_args.dest_name);
+		fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+		fcntl(client_fd, F_SETFL, O_NONBLOCK);
 		while (1) {
+#if KEY_ENABLE
 			size = read(STDIN_FILENO, client_in_buf, BUF_SIZE);
 			if (size > 0) {
 				iv = get_iv();
@@ -70,6 +101,18 @@ int main(int argc, char *argv[])
 				aes_ctr_encrypt(client_in_buf + 8, client_out_buf, iv, size - 8);
 				write(STDOUT_FILENO, client_out_buf, size - 8);
 			}
+#else
+			size = read(STDIN_FILENO, client_in_buf, BUF_SIZE);
+			if (size > 0) {
+				write(client_fd, client_in_buf, size);
+			}
+
+			size = read(client_fd, client_in_buf, BUF_SIZE);
+			if (size > 0) {
+				write(STDOUT_FILENO, client_in_buf, size);
+			}
+
+#endif
 		}
 	}
 	return 0;
