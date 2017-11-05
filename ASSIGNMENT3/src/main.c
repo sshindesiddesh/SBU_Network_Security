2a7d6ccf51df9494ddc883aeccb83517675c9ffe
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-11-04 21:56:39 -0400

    HW3 : Changed Encryption scheme : Send IV only once
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- ASSIGNMENT3/src/main.c
+++ ASSIGNMENT3/src/main.c
@@ -13,6 +13,22 @@
 
 in_args_t in_args;
 
+uint8_t *read_iv(int fd)
+{
+	int size = 0, total = 0;
+	uint8_t *iv = (uint8_t *)malloc(8);
+	fcntl(fd, F_SETFL, O_NONBLOCK);
+	while (1) {
+		total = read(fd, iv + size, 8);
+		if (total > 0) {
+			size += total;
+		}
+		if (size >= 8)
+			break;
+	}
+	return iv;
+}
+
 int main(int argc, char *argv[])
 {
 	arg_flag = 0;
@@ -28,8 +44,8 @@ int main(int argc, char *argv[])
 		uint8_t server_in_buf[BUF_SIZE + 16] = {0};
 		uint8_t server_out_buf[BUF_SIZE + 16] = {0};
 		int size;
-		uint8_t *iv = get_iv();
 		int server_sock = create_serv_sock(in_args.local_port);
+		uint8_t *iv = read_iv(server_sock);
 #if !TRANSP_SERVER
 		int client_sock = create_client_sock(in_args.dest_port, in_args.dest_name);
 		fcntl(client_sock, F_SETFL, O_NONBLOCK);
@@ -39,27 +55,21 @@ int main(int argc, char *argv[])
 		while (1) {
 #if TRANSP_SERVER
 			while ((size = read(STDIN_FILENO, server_in_buf, BUF_SIZE)) >= 0) {
-				iv = get_iv();
-				memcpy(server_out_buf, iv, 8);
-				aes_ctr_encrypt(server_in_buf, server_out_buf + 8, iv, size);
-				write(server_sock, server_out_buf, size + 8);
+				aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
+				write(server_sock, server_out_buf, size);
 			}
-			while ((size = read(server_sock, server_in_buf, BUF_SIZE + 8)) > 0) {
-				memcpy(iv, server_in_buf, 8);
-				aes_ctr_encrypt(server_in_buf + 8, server_out_buf, iv, size - 8);
-				write(STDOUT_FILENO, server_out_buf, size - 8);
+			while ((size = read(server_sock, server_in_buf, BUF_SIZE)) > 0) {
+				aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
+				write(STDOUT_FILENO, server_out_buf, size);
 			}
 #elif KEY_ENABLE
-			while ((size = read(server_sock, server_in_buf, BUF_SIZE + 8)) > 0) {
-					memcpy(iv, server_in_buf, 8);
-					aes_ctr_encrypt(server_in_buf + 8, server_out_buf, iv, size - 8);
-					write(client_sock, server_out_buf, size - 8);
+			while ((size = read(server_sock, server_in_buf, BUF_SIZE)) > 0) {
+					aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
+					write(client_sock, server_out_buf, size);
 			}
 			while ((size = read(client_sock, server_in_buf, BUF_SIZE)) >= 0) {
-					iv = get_iv();
-					memcpy(server_out_buf, iv, 8);
-					aes_ctr_encrypt(server_in_buf, server_out_buf + 8, iv, size);
-					write(server_sock, server_out_buf, size + 8);
+					aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
+					write(server_sock, server_out_buf, size);
 			}
 #else
 			while ((size = read(server_sock, server_in_buf, BUF_SIZE)) >= 0) {
@@ -81,21 +91,19 @@ int main(int argc, char *argv[])
 		int size;
 		uint8_t *iv = get_iv();
 		int client_fd = create_client_sock(in_args.dest_port, in_args.dest_name);
+		write(client_fd, iv, 8);
 		fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
 		fcntl(client_fd, F_SETFL, O_NONBLOCK);
 		while (1) {
 #if KEY_ENABLE
 			while ((size = read(STDIN_FILENO, client_in_buf, BUF_SIZE)) > 0) {
-				iv = get_iv();
-				memcpy(client_out_buf, iv, 8);
-				aes_ctr_encrypt(client_in_buf, client_out_buf + 8, iv, size);
-				write(client_fd, client_out_buf, size + 8);
+				aes_ctr_encrypt(client_in_buf, client_out_buf, iv, size);
+				write(client_fd, client_out_buf, size);
 			}
 
-			while ((size = read(client_fd, client_in_buf, BUF_SIZE + 8)) > 0) {
-				memcpy(iv, client_in_buf, 8);
-				aes_ctr_encrypt(client_in_buf + 8, client_out_buf, iv, size - 8);
-				write(STDOUT_FILENO, client_out_buf, size - 8);
+			while ((size = read(client_fd, client_in_buf, BUF_SIZE)) > 0) {
+				aes_ctr_encrypt(client_in_buf, client_out_buf, iv, size);
+				write(STDOUT_FILENO, client_out_buf, size);
 			}
 #else
 			size = read(STDIN_FILENO, client_in_buf, BUF_SIZE);
