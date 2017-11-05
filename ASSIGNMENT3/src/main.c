4335d7aadc276e15a62f4bc148a3904e866a74ba
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-11-04 22:05:07 -0400

    HW3 : Clean up server and client loops
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- ASSIGNMENT3/src/main.c
+++ ASSIGNMENT3/src/main.c
@@ -29,6 +29,89 @@ uint8_t *read_iv(int fd)
 	return iv;
 }
 
+
+void server_loop(int server_sock)
+{
+	uint8_t server_in_buf[BUF_SIZE + 16] = {0};
+	uint8_t server_out_buf[BUF_SIZE + 16] = {0};
+	int size;
+	uint8_t *iv = read_iv(server_sock);
+#if !TRANSP_SERVER
+	int client_sock = create_client_sock(in_args.dest_port, in_args.dest_name);
+	fcntl(client_sock, F_SETFL, O_NONBLOCK);
+#endif
+	fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+	fcntl(server_sock, F_SETFL, O_NONBLOCK);
+	while (1) {
+#if TRANSP_SERVER
+		while ((size = read(STDIN_FILENO, server_in_buf, BUF_SIZE)) >= 0) {
+			aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
+			write(server_sock, server_out_buf, size);
+		}
+		while ((size = read(server_sock, server_in_buf, BUF_SIZE)) > 0) {
+			aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
+			write(STDOUT_FILENO, server_out_buf, size);
+		}
+#elif KEY_ENABLE
+		while ((size = read(server_sock, server_in_buf, BUF_SIZE)) > 0) {
+				aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
+				write(client_sock, server_out_buf, size);
+		}
+		while ((size = read(client_sock, server_in_buf, BUF_SIZE)) >= 0) {
+				aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
+				write(server_sock, server_out_buf, size);
+		}
+#else
+		while ((size = read(server_sock, server_in_buf, BUF_SIZE)) >= 0) {
+			if (size > 0) {
+				write(client_sock, server_in_buf, size);
+			}
+		}
+		while ((size = read(client_sock, server_in_buf, BUF_SIZE)) >= 0) {
+			if (size > 0) {
+				write(server_sock, server_in_buf, size);
+			}
+		}
+#endif
+	}
+}
+
+void client_loop()
+{
+	char client_in_buf[BUF_SIZE + 16];
+	char client_out_buf[BUF_SIZE + 16];
+	int size;
+	uint8_t *iv = get_iv();
+	int client_fd = create_client_sock(in_args.dest_port, in_args.dest_name);
+	write(client_fd, iv, 8);
+	fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+	fcntl(client_fd, F_SETFL, O_NONBLOCK);
+	while (1) {
+#if KEY_ENABLE
+		while ((size = read(STDIN_FILENO, client_in_buf, BUF_SIZE)) > 0) {
+			aes_ctr_encrypt(client_in_buf, client_out_buf, iv, size);
+			write(client_fd, client_out_buf, size);
+		}
+
+		while ((size = read(client_fd, client_in_buf, BUF_SIZE)) > 0) {
+			aes_ctr_encrypt(client_in_buf, client_out_buf, iv, size);
+			write(STDOUT_FILENO, client_out_buf, size);
+		}
+#else
+		size = read(STDIN_FILENO, client_in_buf, BUF_SIZE);
+		if (size > 0) {
+			write(client_fd, client_in_buf, size);
+		}
+
+		size = read(client_fd, client_in_buf, BUF_SIZE);
+		if (size > 0) {
+			write(STDOUT_FILENO, client_in_buf, size);
+		}
+
+#endif
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	arg_flag = 0;
@@ -41,83 +124,11 @@ int main(int argc, char *argv[])
 
 	/* Server */
 	if (get_flag(ARG_FLAG_LPORT)) {
-		uint8_t server_in_buf[BUF_SIZE + 16] = {0};
-		uint8_t server_out_buf[BUF_SIZE + 16] = {0};
-		int size;
 		int server_sock = create_serv_sock(in_args.local_port);
-		uint8_t *iv = read_iv(server_sock);
-#if !TRANSP_SERVER
-		int client_sock = create_client_sock(in_args.dest_port, in_args.dest_name);
-		fcntl(client_sock, F_SETFL, O_NONBLOCK);
-#endif
-		fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
-		fcntl(server_sock, F_SETFL, O_NONBLOCK);
-		while (1) {
-#if TRANSP_SERVER
-			while ((size = read(STDIN_FILENO, server_in_buf, BUF_SIZE)) >= 0) {
-				aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
-				write(server_sock, server_out_buf, size);
-			}
-			while ((size = read(server_sock, server_in_buf, BUF_SIZE)) > 0) {
-				aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
-				write(STDOUT_FILENO, server_out_buf, size);
-			}
-#elif KEY_ENABLE
-			while ((size = read(server_sock, server_in_buf, BUF_SIZE)) > 0) {
-					aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
-					write(client_sock, server_out_buf, size);
-			}
-			while ((size = read(client_sock, server_in_buf, BUF_SIZE)) >= 0) {
-					aes_ctr_encrypt(server_in_buf, server_out_buf, iv, size);
-					write(server_sock, server_out_buf, size);
-			}
-#else
-			while ((size = read(server_sock, server_in_buf, BUF_SIZE)) >= 0) {
-				if (size > 0) {
-					write(client_sock, server_in_buf, size);
-				}
-			}
-			while ((size = read(client_sock, server_in_buf, BUF_SIZE)) >= 0) {
-				if (size > 0) {
-					write(server_sock, server_in_buf, size);
-				}
-			}
-#endif
-		}
+		server_loop(server_sock);
 	/* Client */
 	} else {
-		char client_in_buf[BUF_SIZE + 16];
-		char client_out_buf[BUF_SIZE + 16];
-		int size;
-		uint8_t *iv = get_iv();
-		int client_fd = create_client_sock(in_args.dest_port, in_args.dest_name);
-		write(client_fd, iv, 8);
-		fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
-		fcntl(client_fd, F_SETFL, O_NONBLOCK);
-		while (1) {
-#if KEY_ENABLE
-			while ((size = read(STDIN_FILENO, client_in_buf, BUF_SIZE)) > 0) {
-				aes_ctr_encrypt(client_in_buf, client_out_buf, iv, size);
-				write(client_fd, client_out_buf, size);
-			}
-
-			while ((size = read(client_fd, client_in_buf, BUF_SIZE)) > 0) {
-				aes_ctr_encrypt(client_in_buf, client_out_buf, iv, size);
-				write(STDOUT_FILENO, client_out_buf, size);
-			}
-#else
-			size = read(STDIN_FILENO, client_in_buf, BUF_SIZE);
-			if (size > 0) {
-				write(client_fd, client_in_buf, size);
-			}
-
-			size = read(client_fd, client_in_buf, BUF_SIZE);
-			if (size > 0) {
-				write(STDOUT_FILENO, client_in_buf, size);
-			}
-
-#endif
-		}
+		client_loop();
 	}
 	return 0;
 }
